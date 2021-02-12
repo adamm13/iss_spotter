49052108f61ea7fd3e894669d44b49ad09d8e287
Adam Mohammed <13.adamm@gmail.com>
2021-02-11 21:58:47 -0500

    went over timebox again, iss promises completed with new index to organize callback hell.

---
+++ iss_promised.js
@@ -0,0 +1,28 @@
+const request = require('request-promise-native');
+
+/*
+ * Requests user's ip address from https://www.ipify.org/
+ * Input: None
+ * Returns: Promise of request for ip data, returned as JSON string
+ */
+
+const fetchIP = (callback) => {
+  return request("https://api.ipify.org?format=json")
+}
+const fetchLocation = (body) => {
+  const ip = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${ip}`);
+};
+const fetchISSFlyOver = (body) => {
+  const location = JSON.parse(body);
+  return request(`http://api.open-notify.org/iss-pass.json?lat=${location.latitude}&lon=${location.longitude}`)
+}
+const nextISSTimesForMyLocation = () => {
+  return fetchIP()
+    .then(fetchLocation)
+    .then(fetchISSFlyOver)   
+}
+
+
+
+module.exports = { nextISSTimesForMyLocation}
